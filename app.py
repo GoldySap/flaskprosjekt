419677eefc106c9bc8ac58bbcf3823318145cec2
Goldsap <92326203+GoldySap@users.noreply.github.com>
2025-11-05 09:05:18 +0100

    soon

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from flask import Flask, render_template, request, redirect
 import dotenv
 import os
 import mariadb
@@ -5,35 +6,91 @@ import functions
 
 dotenv.load_dotenv()
 
+app = Flask(__name__)
+
 #Default Values
-envhost = os.getenv("HOST")
-envuser = os.getenv("USER")
-envpassword = os.getenv("PASSWORD")
-envdatabase = os.getenv("DATABASE")
-envtables = os.getenv("TABLES").split(",")
-envtablecontent = os.getenv("TABLECONTENT").split("|")
-defaulttable = os.getenv("DEFAULTTABLE")
+envhost = os.getenv("DB_HOST")
+envuser = os.getenv("DB_USER")
+envpassword = os.getenv("DB_PASSWORD")
+envdatabase = os.getenv("DB_NAME")
+envtables = os.getenv("DB_TABLES").split(",")
+envtablecontent = os.getenv("DB_TABLECONTENT").split("|")
 
-db = envdatabase
+envdb = envdatabase
 
-try:
-    mydb = mariadb.connect(
-      host = envhost,
-      user = envuser,
-      password = envpassword,
+def get_db_connection():
+    try:
+        mydb = mariadb.connect(
+        host = envhost,
+        user = envuser,
+        password = envpassword,
+        )
+        mycursor = mydb.cursor()
+        functions.dbcheck(mycursor, envdb)
+        mydb = mariadb.connect(
+        host = envhost,
+        user = envuser,
+        password = envpassword,
+        database = envdb,
+        )
+        mycursor = mydb.cursor()
+        functions.tablecheck(mycursor, envtables, envtablecontent)
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB: {e}")
+    return mariadb.connect(
+        host = envhost,
+        user = envuser,
+        password = envpassword,
+        database = envdb
     )
-    print(mydb)
-    print("Connected successfully!")
-    mycursor = mydb.cursor()
-    functions.dbcheck(mycursor, db)
-    mydb = mariadb.connect(
-      host = envhost,
-      user = envuser,
-      password = envpassword,
-      database = db,
-    )
-    print(f"Connected to {db} successfully!")
-    mycursor = mydb.cursor()
-    functions.tablecheck(mycursor, envtables, envtablecontent)
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB: {e}")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/users')
+def users():
+    mydb = get_db_connection()
+    cursor = mydb.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM customers")
+    result = cursor.fetchall()
+    mydb.close()
+    return render_template('test.html', users=result)
+
+@app.route('/users/update', methods=['POST'])
+def update():
+    cid = request.form['id']
+    name = request.form['name']
+    address = request.form['address']
+    sql = "UPDATE users SET name=%s, address=%s WHERE id=%s"
+    val = (name, address, cid)
+    mydb = get_db_connection()
+    cursor = mydb.cursor()
+    cursor.execute(sql, val)
+    mydb.commit()
+    mydb.close()
+    return redirect('/')
+
+@app.route('/users/delete', methods=['POST'])
+def delete():
+    cid = request.form['id']
+    sql = "DELETE FROM customers WHERE id=%s"
+    val = (cid)
+    mydb = get_db_connection()
+    cursor = mydb.cursor()
+    cursor.execute(sql, val)
+    mydb.commit()
+    mydb.close()
+    return redirect('/')
+
+@app.route('/products')
+def products():
+    mydb = get_db_connection()
+    cursor = mydb.cursor(dictionary=True)
+    cursor.execute("SELECT name, pris FROM products")
+    result = cursor.fetchall()
+    mydb.close()
+    return render_template('products.html', users=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
